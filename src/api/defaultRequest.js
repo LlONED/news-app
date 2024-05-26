9951c90f93c779e39e50f0d9a0ee20c5e949b3a1
LlONED <alex5000843@gmail.com>
2024-05-27 00:21:28 +0300

    refactor(defaultRequest): changed base url from http to https

--- src/api/defaultRequest.js
+++ src/api/defaultRequest.js
@@ -4,7 +4,7 @@ export function defaultRequest(uri, options) {
   if (import.meta.env["VITE_MOCK_API_NEWS"] === "true") {
     return new Promise((res) => setTimeout(() => res(mocks[uri]), 1000));
   } else {
-    return fetch("http://flems.github.io/test/api" + uri, options).then(
+    return fetch("https://flems.github.io/test/api" + uri, options).then(
       (data) => {
         return data.json();
       }
